66ce7fcce7451a982c7d718012f711cdf85ae730
Per Manniche Bering <pmbering@gmail.com>
2015-09-27 22:28:46 +0200

    Missed file

---
+++ src/Lightcore/Kernel/Pipeline/Processor.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Lightcore.Kernel.Pipeline
+{
+    public abstract class Processor
+    {
+        public virtual Task ProcessAsync(PipelineArgs args)
+        {
+            return Task.Run(() => Process(args));
+        }
+
+        public virtual void Process(PipelineArgs args)
+        {
+        }
+    }
+}
