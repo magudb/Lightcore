ffb1a89718dc209b1055f19e7d377edebbf09587
Per Manniche Bering <pmbering@gmail.com>
2015-10-09 16:44:38 +0200

    Fixed path

--- src/Lightcore/Kernel/Pipeline/Request/Processor/ResolveContentPathProcessor.cs
+++ src/Lightcore/Kernel/Pipeline/Request/Processor/ResolveContentPathProcessor.cs
@@ -8,7 +8,7 @@ namespace Lightcore.Kernel.Pipeline.Request.Processor
         {
             var requestArgs = (RequestArgs)args;
             var context = args.Context.LightcoreContext();
-            var contentPath = string.Format("{0}/{1}", requestArgs.Config.StartItem.TrimEnd('/'), args.Context.Request.Path.Value).ToLowerInvariant();
+            var contentPath = string.Format("{0}/{1}", requestArgs.Config.StartItem.TrimEnd('/'), args.Context.Request.Path.Value.TrimStart('/')).TrimEnd('/').ToLowerInvariant();
 
             const string mustStartWith = "/sitecore/content/";
 
