d0163f69495886ac249ac9299b691ed64348154b
Per Manniche Bering <pmbering@gmail.com>
2015-10-11 20:31:13 +0200

    Added support for static files

--- src/Lightcore/Kernel/Pipelines/Request/Processors/FilterRequestProcessor.cs
+++ src/Lightcore/Kernel/Pipelines/Request/Processors/FilterRequestProcessor.cs
@@ -5,8 +5,6 @@ namespace Lightcore.Kernel.Pipelines.Request.Processors
         public override void Process(PipelineArgs args)
         {
             var requestArgs = (RequestArgs)args;
-
-            // TODO: Look into why we get requests to forexample "favicon.ico" while static files is not enabled?
             var ignoreRequest = requestArgs.HttpContext.Request.Path.Value.Contains(".");
 
             if (ignoreRequest)
