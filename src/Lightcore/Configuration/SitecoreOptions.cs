e2481f78045268371f36e5e214554ee9bdcf2f20
Per Manniche Bering <pmbering@gmail.com>
2015-10-23 23:55:30 +0200

    Added missing file

---
+++ src/Lightcore/Configuration/SitecoreOptions.cs
@@ -0,0 +1,28 @@
+﻿namespace Lightcore.Configuration
+{
+    public class SitecoreOptions
+    {
+        public string StartItem { get; set; }
+        public string Database { get; set; }
+        public string Device { get; set; }
+        public string Cdn { get; set; }
+
+        public void Verify()
+        {
+            if (string.IsNullOrEmpty(StartItem))
+            {
+                throw new InvalidConfigurationException($"{nameof(StartItem)} was null or empty");
+            }
+
+            if (string.IsNullOrEmpty(Database))
+            {
+                throw new InvalidConfigurationException($"{nameof(Database)} was null or empty");
+            }
+
+            if (string.IsNullOrEmpty(Device))
+            {
+                throw new InvalidConfigurationException($"{nameof(Device)} was null or empty");
+            }
+        }
+    }
+}
